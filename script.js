43dd35ec8b450f03e6fc0945cec603c9687856e9
modanishmansuri001-oss <mo.danishmansuri001@gmail.com>
2026-08-13 21:08:26 +0530

    Implement password visibility toggle feature
    Add functionality to toggle password visibility.

--- script.js
+++ script.js
@@ -61,3 +61,16 @@ if (menuBtn) {
     });
 
 }
+const pass = document.getElementById("password");
+
+const eye = document.querySelector(".password-group .right-icon img");
+
+eye.onclick = function () {
+    if (pass.type === "password") {
+        pass.type = "text";
+        eye.src = "images/eye-off.svg";
+    } else {
+        pass.type = "password";
+        eye.src = "images/eye.svg";
+    }
+};
