99dc01f55f0f41fc79fcef3a7a4c6646054c0bc3
modanishmansuri001-oss <mo.danishmansuri001@gmail.com>
2026-08-17 23:18:34 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,35 @@
 
+// =====================================================
+// 01. HOME → LOGIN PAGE
+// =====================================================
+
+const loginBtn = document.getElementById("loginBtn");
+
+if (loginBtn) {
+    loginBtn.addEventListener("click", function () {
+
+        document.body.classList.remove("home-mode");
+        document.body.classList.add("login-mode");
+
+        window.scrollTo(0, 0);
+
+    });
+}
+
+
+// =====================================================
+// 02. LOGIN → HOME PAGE
+// =====================================================
+
+const menuBtn = document.querySelector("#loginPage .menu-btn");
+
+if (menuBtn) {
+    menuBtn.addEventListener("click", function () {
+
+        document.body.classList.remove("login-mode");
+        document.body.classList.add("home-mode");
+
+        window.scrollTo(0, 0);
+
+    });
+}
